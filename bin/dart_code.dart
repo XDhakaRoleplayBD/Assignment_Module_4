757d9a43cc86b02b39d9fe9b632e641df1785395
AL SABA SADDAT <2222081070@uttarauniversity.edu.bd>
2026-09-01 22:19:10 +0600

    মেনু একবার দেখানোর জন্য লুপের বাইরে মেনু নিয়েছি

--- bin/dart_code.dart
+++ bin/dart_code.dart
@@ -1,18 +1,18 @@
 import 'dart:io';
 main(){
   String y_n = "";
+  print("====================================");
+  print("    SMART TEMPERATURE CONVERTER    ");
+  print("====================================");
+  print("");
+  print("1. Celsius to Fahrenheit");
+  print("2. Fahrenheit to Celsius");
+  print("3. Celsius to Kelvin");
+  print("4. Kelvin to Celsius");
+  print("5. Fahrenheit to Kelvin");
+  print("6. Kelvin to Fahrenheit");
+  print("7. Exit");
   do {
-    print("====================================");
-    print("    SMART TEMPERATURE CONVERTER    ");
-    print("====================================");
-    print("");
-    print("1. Celsius to Fahrenheit");
-    print("2. Fahrenheit to Celsius");
-    print("3. Celsius to Kelvin");
-    print("4. Kelvin to Celsius");
-    print("5. Fahrenheit to Kelvin");
-    print("6. Kelvin to Fahrenheit");
-    print("7. Exit");
     print("");
     stdout.write("Enter your choice : ");
     int cho = int.parse(stdin.readLineSync()!);
