10881e048fcc89b00bcb93eefbe88cca4b68ead9
AL SABA SADDAT <2222081070@uttarauniversity.edu.bd>
2026-09-10 00:19:23 +0600

    Assignment 3

--- bin/dart_code.dart
+++ bin/dart_code.dart
@@ -1,83 +1,53 @@
-import 'dart:io';
+import "dart:io";
+
+class BankAccount {
+  String Account_Holder_Name;
+  String Account_Number;
+  num _Balance;
+
+  BankAccount(this.Account_Holder_Name, this.Account_Number, this._Balance);
+
+  deposit() {
+    stdout.write("Input the Deposit Ammount : ");
+    num diposit_amount = num.parse(stdin.readLineSync()!);
+    if (diposit_amount > 0) {
+      _Balance += diposit_amount;
+      print("After Deposit: Balance: $_Balance");
+    }
+    else {
+      print("Incorrect Diposit Ammount");
+    }
+  }
+
+  withdraw() {
+    stdout.write("Input the Withdraw Ammount : ");
+    num withdraw_amount = num.parse(stdin.readLineSync()!);
+    if (withdraw_amount <= 0){
+      print("Incorrect Ammount");
+    }
+    else{
+      if (withdraw_amount <= _Balance) {
+        _Balance -= withdraw_amount;
+        print("After Withdrawal: Balance: $_Balance");
+      }
+      else {
+        print("Insufficient Balance");
+      }
+    }
+    }
+  displayAccountInfo(){
+    print("Account Holder: $Account_Holder_Name Account Number: $Account_Number Balance: $_Balance");
+  }
+}
+
 main(){
-  String y_n = "";
-  print("====================================");
-  print("    SMART TEMPERATURE CONVERTER    ");
-  print("====================================");
-  print("");
-  print("1. Celsius to Fahrenheit");
-  print("2. Fahrenheit to Celsius");
-  print("3. Celsius to Kelvin");
-  print("4. Kelvin to Celsius");
-  print("5. Fahrenheit to Kelvin");
-  print("6. Kelvin to Fahrenheit");
-  print("7. Exit");
-  do {
-    print("");
-    stdout.write("Enter your choice : ");
-    int cho = int.parse(stdin.readLineSync()!);
-    print("");
-    if (cho == 1) {
-      stdout.write("Enter temperature in Celsius : ");
-      double c = double.parse(stdin.readLineSync()!);
-      double f = (c * (9 / 5)) + 32;
-      print("$c°C = $f°F");
-      print("");
-      stdout.write("Do you want to perform another conversion? (y/n):");
-    }
-    else if (cho == 2) {
-      stdout.write("Enter temperature in Fahrenheit : ");
-      double f = double.parse(stdin.readLineSync()!);
-      double c = (f - 32) * (5 / 9);
-      print("$f°F = $c°C");
-      print("");
-      stdout.write("Do you want to perform another conversion? (y/n):");
-    }
-    else if (cho == 3) {
-      stdout.write("Enter temperature in Celsius : ");
-      double c = double.parse(stdin.readLineSync()!);
-      double k = c + 273.15;
-      print("$c°C = $k K");
-      print("");
-      stdout.write("Do you want to perform another conversion? (y/n):");
-    }
-    else if (cho == 4) {
-      stdout.write("Enter temperature in Kelvin : ");
-      double k = double.parse(stdin.readLineSync()!);
-      double c = k - 273.15;
-      print("$k K = $c°C");
-      print("");
-      stdout.write("Do you want to perform another conversion? (y/n):");
-    }
-    else if (cho == 5) {
-      stdout.write("Enter temperature in Fahrenheit : ");
-      double f = double.parse(stdin.readLineSync()!);
-      double k = (f - 32) * (5 / 9) + 273.15;
-      print("$f°F = $k K");
-      print("");
-      stdout.write("Do you want to perform another conversion? (y/n):");
-    }
-    else if (cho == 6) {
-      stdout.write("Enter temperature in Kelvin : ");
-      double k = double.parse(stdin.readLineSync()!);
-      double f = (k - 273.15) * (9 / 5) + 32;
-      print("$k K = $f°F");
-      print("");
-      stdout.write("Do you want to perform another conversion? (y/n):");
-    }
-    else if (cho == 7) {
-      print("");
-      print("====================================");
-      print("     Thank You!");
-      print("     Program Ended");
-      print("====================================");
-      exit(0);
-    }
-    y_n = stdin.readLineSync()!.toLowerCase();
-  }while (y_n != "n");
-  print("");
-  print("====================================");
-  print("     Thank You!");
-  print("     Program Ended");
-  print("====================================");
+  // Support class এ বলেছে input নিতে, input নিলে কোনো problem নেই এবং Number কাটবে না, তাই input নিয়েছি
+  BankAccount Object1 = BankAccount("Rahim", "1001", 5000);
+  Object1.displayAccountInfo();
+  Object1.deposit();
+  Object1.withdraw();
+  BankAccount Object2 = BankAccount("Karim", "1002", 8000);
+  Object2.displayAccountInfo();
+  Object2.deposit();
+  Object2.withdraw();
 }
